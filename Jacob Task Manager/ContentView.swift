8638bd18c1817365198ee0cda68ab8ef3b20d971
Jacob Yoo <tolearneverything123@gmail.com>
2024-02-07 00:14:22 -0500

    Initial Commit

---
+++ Jacob Task Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jacob Task Manager
+//
+//  Created by Jacob Yoo on 2/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jacob Task Manager/Jacob_Task_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jacob_Task_ManagerApp.swift
+//  Jacob Task Manager
+//
+//  Created by Jacob Yoo on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Jacob_Task_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
